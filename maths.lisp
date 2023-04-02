ef3d3afa7e59927583eab644bdb7df7b72d279be
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-02 16:58:35 +0100

    Update and rename knight.lisp to maths.lisp

---
+++ maths.lisp
@@ -0,0 +1,15 @@
+;;
+;; Triple the value of a number
+;;
+
+(defun triple (X)
+  "Compute three times X."  ; Inline comments can
+  (* 3 X))                  ; be placed here.
+
+;;
+;; Negate the sign of a number
+;;
+
+(defun negate (X)
+  "Negate the value of X."  ; This is a documentation string.
+  (- X)) 
